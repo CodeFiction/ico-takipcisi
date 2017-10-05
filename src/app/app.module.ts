4e0aa5957ae037a0c2fd1560d0fbc2bdecc354ae
msusur <mail@mertsusur.com>
2017-10-05 23:57:18 +0100

    Initial Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { MdMenuModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MainComponent } from './components/main/main.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    BannerComponent,
+    MainComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
