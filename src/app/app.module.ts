72cb851b8e5672dc4416a1a90e157cf4d7ea363b
msusur <mail@mertsusur.com>
2017-10-05 23:29:38 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MdMenuModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
